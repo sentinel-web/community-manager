89ee766a7393af09b2c24993480f7b4de98e86b3
Oke Johannsen <oke.johannsen@protonmail.com>
2024-08-25 16:28:55 +0200

    extended app config and settings

---
+++ imports/ui/section-card/SectionCard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Card, Col, Row, Typography } from "antd";
+
+export default function SectionCard({ children, title, ready }) {
+  return (
+    <Card
+      loading={!ready}
+      type="inner"
+      title={<SectionCardTitle title={title} />}
+    >
+      {children}
+    </Card>
+  );
+}
+
+function SectionCardTitle({ title }) {
+  return (
+    <Row gutter={[16, 16]}>
+      <Col span={24}>
+        <Typography.Title level={2}>{title}</Typography.Title>
+      </Col>
+    </Row>
+  );
+}
--- imports/ui/settings/settings.hook.jsx
+++ imports/ui/settings/settings.hook.jsx
@@ -4,15 +4,19 @@ import { useTracker } from "meteor/react-meteor-data";
 import SettingsCollection from "../../api/collections/settings.collection";
 
 export default function useSettings() {
-  const { ready, communityTitle, communityLogo } = useTracker(() => {
-    const subscription = Meteor.subscribe("settings");
-    return {
-      ready: subscription.ready(),
-      communityTitle: SettingsCollection.findOne({ key: "community-title" })
-        ?.value,
-      communityLogo: SettingsCollection.findOne({ key: "community-logo" })
-        ?.value,
-    };
-  }, []);
-  return { ready, communityTitle, communityLogo };
+  const { ready, communityTitle, communityLogo, communityColor } =
+    useTracker(() => {
+      const subscription = Meteor.subscribe("settings");
+      return {
+        ready: subscription.ready(),
+        communityTitle: SettingsCollection.findOne({ key: "community-title" })
+          ?.value,
+        communityLogo: SettingsCollection.findOne({ key: "community-logo" })
+          ?.value,
+        communityColor:
+          SettingsCollection.findOne({ key: "community-color" })?.value ??
+          "#708152",
+      };
+    }, []);
+  return { ready, communityTitle, communityLogo, communityColor };
 }
