93599beb532857ac099c5b2a90d12ce9c0fcdd11
Oke Johannsen <oke.johannsen@protonmail.com>
2024-08-27 19:43:18 +0200

    - introduced layout component - added drawer component and context - adjusted css to maintain layout
    relative logo scaling and package updates
    
    test commit
    
    test commit
    
    test commit
    
    test commit
    
    test commit
    
    test commit
    
    test commit
    
    fix for accept

--- imports/ui/footer/Footer.jsx
+++ imports/ui/footer/Footer.jsx
@@ -1,5 +1,12 @@
+import { Col, Row } from "antd";
 import React from "react";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <Row gutter={[16, 16]}>
+        <Col>© {new Date().getFullYear()}</Col>
+      </Row>
+    </footer>
+  );
 }
