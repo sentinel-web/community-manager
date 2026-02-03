899ca7244aeb566788ba5bc56b2f5c5aaa95976a
Oke Johannsen <oke.johannsen@protonmail.com>
2026-02-03 13:02:23 +0100

    Add PropTypes and JSDoc documentation to components
    - Add PropTypes to Table component for columns and datasource props
    - Add JSDoc documentation to Members, Logs, and Navigation components
    - Add JSDoc documentation to column factory functions:
      - logs.columns.jsx
      - event.columns.jsx
      - eventTypes.columns.jsx
      - roles.columns.jsx
      - medals.columns.jsx
      - ranks.columns.jsx
      - members.columns.jsx
    
    Closes #52

--- imports/ui/table/Table.jsx
+++ imports/ui/table/Table.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
 import { Table as AntdTable } from 'antd';
+import PropTypes from 'prop-types';
+import React from 'react';
 
 export default function Table({ columns, datasource }) {
   return <AntdTable columns={columns} dataSource={datasource} pagination={false} rowKey="_id" />;
 }
+Table.propTypes = {
+  columns: PropTypes.array,
+  datasource: PropTypes.array,
+};
