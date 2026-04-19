fe70487da1c11beb86e6d0d263d9f4551b2e7784
Oke Johannsen <oke.johannsen@protonmail.com>
2026-04-19 15:11:41 +0200

    refactor(server): migrate registrations API to TypeScript

--- server/apis/registrations.server.ts
+++ server/apis/registrations.server.ts
@@ -5,42 +5,42 @@ import { validateNumber } from '../main';
 
 if (Meteor.isServer) {
   Meteor.methods({
-    'registrations.validateId': async function (id = '', excludeId = false) {
+    'registrations.validateId': async function (id: number = 0, excludeId: string | false = false): Promise<boolean> {
       validateNumber(id, false);
 
-      const filter = { $and: [{ 'profile.id': id }] };
+      const filter: Record<string, unknown> = { $and: [{ 'profile.id': id }] };
       if (this.userId && excludeId) {
-        filter.$and.push({ 'profile.id': { $ne: id } });
+        (filter.$and as Record<string, unknown>[]).push({ 'profile.id': { $ne: id } });
       }
       const matchingMembers = await MembersCollection.findOneAsync(filter);
 
-      const registrationFilter = {
+      const registrationFilter: Record<string, unknown> = {
         $and: [{ id: id }],
       };
       if (this.userId && excludeId) {
-        registrationFilter.$and.push({ _id: { $ne: excludeId } });
+        (registrationFilter.$and as Record<string, unknown>[]).push({ _id: { $ne: excludeId } });
       }
       const matchingRegistrations = await RegistrationsCollection.findOneAsync(registrationFilter);
       return !(matchingMembers || matchingRegistrations);
     },
-    'registrations.validateName': async function (name = '', excludeId = false) {
+    'registrations.validateName': async function (name: string = '', excludeId: string | false = false): Promise<boolean> {
       if (!name || typeof name !== 'string') {
         throw new Meteor.Error('invalid-name', 'Invalid name', name);
       }
 
-      const filter = {
+      const filter: Record<string, unknown> = {
         $and: [{ 'profile.name': name }],
       };
       if (this.userId && excludeId) {
-        filter.$and.push({ 'profile.name': { $ne: name } });
+        (filter.$and as Record<string, unknown>[]).push({ 'profile.name': { $ne: name } });
       }
       const matchingMembers = await MembersCollection.findOneAsync(filter);
 
-      const registrationFilter = {
+      const registrationFilter: Record<string, unknown> = {
         $and: [{ name: name }],
       };
       if (this.userId && excludeId) {
-        registrationFilter.$and.push({ _id: { $ne: excludeId } });
+        (registrationFilter.$and as Record<string, unknown>[]).push({ _id: { $ne: excludeId } });
       }
       const matchingRegistrations = await RegistrationsCollection.findOneAsync(registrationFilter);
       return !(matchingMembers || matchingRegistrations);
